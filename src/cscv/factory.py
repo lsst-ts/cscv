3a79b7a20b1b623b5e15dc0091441c0b7b49de23
Amanda Ibsen <amanda.ibsen@gmail.com>
2025-04-24 14:08:21 -0400

    Create factory.

---
+++ src/cscv/factory.py
@@ -0,0 +1,29 @@
+"""Component factory for the application."""
+
+from structlog.stdlib import BoundLogger
+
+from .config import config
+from .services.obsenv_manager_service import CSCVService
+from .storage.fake_obsenv_store import FakeCSCVStore
+from .storage.obsenv_store import CSCVStore
+from .storage.store import Store
+
+
+class Factory:
+    """A factory for the application components."""
+
+    def __init__(self, *, logger: BoundLogger) -> None:
+        self.logger = logger
+
+    def create_obsenv_manager_service(self) -> CSCVService:
+        """Create a cscv service."""
+        return CSCVService(
+            logger=self.logger, obsenv_store=self.create_cscv_store()
+        )
+
+    def create_cscv_store(self) -> Store:
+        """Create a cscv store."""
+        if config.use_fake_obsenv_manager:
+            return FakeCSCVStore(logger=self.logger)
+        else:
+            return CSCVStore(logger=self.logger)
