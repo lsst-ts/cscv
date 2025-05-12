c78fd70741e950b29174273d50b818291f8d2221
Amanda Ibsen <amanda.ibsen@gmail.com>
2025-05-12 17:27:26 -0400

    Modify parser.

--- src/cscv/storage/output_parser.py
+++ src/cscv/storage/output_parser.py
@@ -21,7 +21,7 @@ class OutputParser:
         """Parse desired/current key=value strings into CSCInformation."""
         # construct dict from current version dict
         cscv_dict = {
-            key.split(".")[2].lower(): [value]
+            key.split(".")[2].lower(): [value, "no data"]
             for key, value in current.items()
         }
         # update dict with desired versions
@@ -33,15 +33,13 @@ class OutputParser:
                 key, value = line_clean.split("=", maxsplit=1)
                 key = key.split("ts_")[-1]
                 if key in cscv_dict:
-                    cscv_dict[key].append(value)
-        csc_list = []
-        for k, _ in cscv_dict:
-            csc_list.append(
-                CSCInformation(
-                    name=k,
-                    desired_version=cscv_dict[k][1],
-                    current_version=cscv_dict[k][0],
-                )
-            )
+                    cscv_dict[key][1] = value
 
-        return csc_list
+        return [
+            CSCInformation(
+                name=k,
+                desired_version=cscv_dict[k][1],
+                current_version=cscv_dict[k][0],
+            )
+            for k in cscv_dict
+        ]
